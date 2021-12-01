8cb65f9ecc6cdfda81eb4c672a9b1b938e1e9f2b
Jhosef Marks de Carvalho <jhosef@gmail.com>
2021-12-01 11:15:59 -0300

    lesson010

---
+++ src/com/jhosefmarks/basic/lesson010/Variables.java
@@ -0,0 +1,19 @@
+package com.jhosefmarks.basic.lesson010;
+
+public class Variables {
+
+	public static void main(String[] args) {
+//		int age;
+//		String name;
+//		String dogName;
+
+		int age = 35;
+		String name = "Jhosef Marks";
+		String dogName = "Lily";
+
+		System.out.println("My name is " + name + " and i am " + age + " years old.");
+		System.out.println(dogName + " is my dog.");
+
+	}
+
+}

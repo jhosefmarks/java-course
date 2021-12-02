c3fe43124405c6da27d9e46d25d27fbfc2840ecb
Jhosef Marks de Carvalho <jhosef@gmail.com>
2021-12-02 00:13:33 -0300

    lesson012

---
+++ src/com/jhosefmarks/basic/lesson012/Keyboard.java
@@ -0,0 +1,32 @@
+package com.jhosefmarks.basic.lesson012;
+
+import java.util.Scanner;
+
+public class Keyboard {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("What's your first name?");
+		String firstName = scan.next();
+		// Workaround: use this because next() doesn't read the newline character in your input
+		scan.nextLine(); 
+				
+		System.out.println("What's your full name?");
+		String fullName = scan.nextLine();
+		
+		System.out.println("How old are you?");
+		int age = scan.nextInt();
+		
+		System.out.println("How tall are you?");
+		double height = scan.nextDouble();
+		
+		System.out.println("Your first name is " + firstName);
+		System.out.println("Your full name is " + fullName);
+		System.out.println("Your are " + age + " years old.");
+		System.out.println("Your are " + height	 + " tall.");
+	
+	}
+
+}
